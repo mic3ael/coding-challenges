e68bb2eef6e4bced4010f3256f3c894e8f39727b
Michael Horojanski <mic3ael@gmail.com>
2023-09-27 06:00:33 +0300

    time: O(n), space: O(1)

--- algoexpert/isMonotonic.js
+++ algoexpert/isMonotonic.js
@@ -1,23 +1,12 @@
 // time: O(n), space: O(1)
 function isMonotonic(array) {
-  if (!array.length || array.length === 1) return true;
-  let isMono = true;
-  // increasing
+  let isNonDesc = true;
+  let isNonIncr = true;
+
   for (let i = 1; i < array.length; i++) {
-    if (!isMono) break;
-    if (array[i] >= array[i - 1]) continue;
-    isMono = false;
+    if (array[i] > array[i - 1]) isNonDesc = false;
+    if (array[i] < array[i - 1]) isNonIncr = false;
   }
 
-  if (isMono) return true;
-
-  // descreasing
-  isMono = true;
-  for (let i = 1; i < array.length; i++) {
-    if (!isMono) break;
-    if (array[i] <= array[i - 1]) continue;
-    isMono = false;
-  }
-
-  return isMono;
+  return isNonDesc || isNonIncr;
 }
