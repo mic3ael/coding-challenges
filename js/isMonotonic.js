e81c7d548ebabb1028eba7163ffed545ccff54ab
Michael Horojanski <mic3ael@gmail.com>
2023-09-27 05:00:43 +0300

    time: O(n), space: O(1)

---
+++ js/isMonotonic.js
@@ -0,0 +1,22 @@
+function isMonotonic(array) {
+  if (!array.length || array.length === 1) return true;
+  let isMono = true;
+  // increasing
+  for (let i = 1; i < array.length; i++) {
+    if (!isMono) break;
+    if (array[i] >= array[i - 1]) continue;
+    isMono = false;
+  }
+
+  if (isMono) return true;
+
+  // descreasing
+  isMono = true;
+  for (let i = 1; i < array.length; i++) {
+    if (!isMono) break;
+    if (array[i] <= array[i - 1]) continue;
+    isMono = false;
+  }
+
+  return isMono;
+}
