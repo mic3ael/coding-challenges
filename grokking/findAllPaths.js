bd177c950b17bdf5a9a2e29abd9941181eea45ac
Michael Horojanski <mic3ael@gmail.com>
2024-02-12 14:45:05 +0200

    time: O(n), space: O(n*logN) - when n is the number of nodes in the tree, logN when the current path is stored per node.

---
+++ grokking/findAllPaths.js
@@ -0,0 +1,41 @@
+/*class TreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}*/
+
+class Solution {
+  // time: O(n), space: O(n*logN) - when n is the number of nodes in the tree, logN when the current path is stored per node.
+  findPaths(root, sum) {
+    const allPaths = [];
+    const stack = [{ node: root, path: [], currentSum: 0 }];
+
+    while (stack.length) {
+      const { node, path, currentSum } = stack.pop();
+      if (node.left) {
+        stack.push({
+          node: node.left,
+          path: [...path, node.val],
+          currentSum: currentSum + node.val,
+        });
+      }
+
+      if (node.right) {
+        stack.push({
+          node: node.right,
+          path: [...path, node.val],
+          currentSum: currentSum + node.val,
+        });
+      }
+
+      if (!node.left && !node.right && currentSum + node.val === sum) {
+        path.push(node.val);
+        allPaths.push(path);
+      }
+    }
+
+    return allPaths;
+  }
+}
