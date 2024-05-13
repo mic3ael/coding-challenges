b964e6fa4e52ce6b4771b91baf9b8842a2fba8c7
Michael Horojanski <mhorojanski@chegg.com>
2024-05-13 19:40:55 +0300

    time: O(n), space: O(1)

---
+++ grokking/canPlaceFlowers.js
@@ -0,0 +1,27 @@
+class Solution {
+  // time: O(n), space: O(1)
+  canPlaceFlowers(flowerbed, n) {
+    if (!flowerbed.length) return false;
+    for (let i = 0; i < flowerbed.length; i++) {
+      if (i == 0 && flowerbed[i] != 1 && flowerbed[i + 1] != 1) {
+        flowerbed[i] = 1;
+        n--;
+      }
+      if (
+        i == flowerbed.length - 1 &&
+        flowerbed[i] != 1 &&
+        flowerbed[i - 1] != 1
+      ) {
+        flowerbed[i] = 1;
+        n--;
+      }
+
+      if (flowerbed[i] != 1 && flowerbed[i - 1] != 1 && flowerbed[i + 1] != 1) {
+        flowerbed[i] = 1;
+        n--;
+      }
+    }
+
+    return n <= 0 ? true : false;
+  }
+}
