85f408cc9a249bfc76e645237dbf81eb353ad843
Michael Horojanski <mhorojanski@chegg.com>
2024-05-14 23:12:11 +0300

    time: O(n), space: O(log n)

--- grokking/hasPath.js
+++ grokking/hasPath.js
@@ -23,4 +23,22 @@ class Solution {
 
     return false;
   }
+  // time: O(n), space: O(log n) -> when n is the number of nodes in the tree
+  hasPath2(root, sum) {
+    return this.dfs(root, 0, sum);
+  }
+  dfs(node, currentSum, targetSum) {
+    if (currentSum + node.val === targetSum && !node.left && !node.right)
+      return true;
+    if (currentSum > targetSum) return false;
+
+    let left;
+    if (node.left) left = this.dfs(node.left, currentSum + node.val, targetSum);
+
+    let right;
+    if (node.right)
+      right = this.dfs(node.right, currentSum + node.val, targetSum);
+
+    return !!left || !!right;
+  }
 }
