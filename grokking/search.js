56a499cb7285f8f0ff6b36ffc6973205547d0557
Michael Horojanski <mhorojanski@chegg.com>
2024-05-15 09:10:04 +0300

    time: O(n), space: O(n)

---
+++ grokking/search.js
@@ -0,0 +1,20 @@
+class Solution {
+  // time: O(n), space: O(n) -> where n is the length of the input arr.
+  search(arr, targetSum) {
+    const visited = new Map();
+
+    for (let i = 0; i < arr.length; i++) {
+      if (!visited.has(arr[i])) visited.set(arr[i], i);
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      if (visited.has(targetSum - arr[i])) {
+        const idx = visited.get(targetSum - arr[i]);
+        if (idx == i) continue;
+        return [idx, i];
+      }
+    }
+
+    return [-1, -1];
+  }
+}
