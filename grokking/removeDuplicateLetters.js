9f5fd6a1bfc7f137be75b68ac88e3660ff35b4d0
Michael Horojanski <mic3ael@gmail.com>
2024-06-04 08:52:30 +0300

    time: O(n), space: O(1)

---
+++ grokking/removeDuplicateLetters.js
@@ -0,0 +1,38 @@
+class Solution {
+  removeDuplicateLetters(s) {
+    const count = new Map();
+    const visited = new Set();
+    const result = [];
+
+    for (let c of s) {
+      if (!count.has(c)) count.set(c, 0);
+      count.set(c, count.get(c) + 1);
+    }
+
+    for (let i = 0; i < s.length; i++) {
+      const char = s.charAt(i);
+      if (visited.has(char)) {
+        count.set(char, count.get(char) - 1);
+        continue;
+      }
+
+      while (
+        result.length &&
+        char < result[result.length - 1] &&
+        count.get(result[result.length - 1]) > 0
+      ) {
+        visited.delete(result.pop());
+      }
+      result.push(char);
+      visited.add(char);
+      count.set(char, count.get(char) - 1);
+    }
+
+    return result.join('');
+  }
+}
+
+let sol = new Solution();
+console.log(sol.removeDuplicateLetters('bbaac')); // Output: "bac"
+console.log(sol.removeDuplicateLetters('zabccde')); // Output: "zabcde"
+console.log(sol.removeDuplicateLetters('mnopmn')); // Output: "mnop"
