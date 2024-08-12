a7d5ccdbd6ae43fc93fb17595af0f00c0cdb2da6
Michael <mic3ael@gmail.com>
2024-08-12 19:49:04 +0300

    Time: 58 ms (62.28%), Space: 51.3 MB (43.97%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -10,12 +10,39 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var rightSideView = function(root, level = 0, result = []) {
+var rightSideView2 = function(root, level = 0, result = []) {
     if(root == null) return result;
     
     if(result.length < level + 1)
         result.push(root.val);
-    rightSideView(root.right, level+1, result);
-    rightSideView(root.left, level+1, result);
+    rightSideView2(root.right, level+1, result);
+    rightSideView2(root.left, level+1, result);
+    return result;
+};
+
+var rightSideView = function(root) {
+    const result = [];
+    if(root == null) return result;
+    const queue = [root];
+    let nodesInTheLevel = 1;
+    let currentLevel = 0;
+    while(queue.length){
+        const node = queue.shift();
+        nodesInTheLevel--;
+        if(node.right)
+            queue.push(node.right);
+        if(node.left)
+            queue.push(node.left);
+        
+        if(result.length < currentLevel + 1)
+            result.push(node.val);
+        
+        if(nodesInTheLevel == 0) {
+            nodesInTheLevel = queue.length;
+            currentLevel++;
+        }
+        
+    }
+
     return result;
 };
