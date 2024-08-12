c2e75e776fd2f4b4093912a5d2279a2fe2097b3f
Michael <mic3ael@gmail.com>
2024-08-12 09:56:40 +0300

    Time: 75 ms (68.83%), Space: 54.6 MB (62.34%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -10,12 +10,29 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function(root, level = 1, result = []) {
+var levelOrder2 = function(root, level = 1, result = []) {
     if(root == null) return result;
     if(result.length < level)
         result.push([]);
     result[level-1].push(root.val);
-    levelOrder(root.left, level+1, result);
-    levelOrder(root.right, level+1, result);
+    levelOrder2(root.left, level+1, result);
+    levelOrder2(root.right, level+1, result);
+    return result;
+};
+
+var levelOrder = function(root) {
+    const queue = [{node: root, level: 0}];
+    const result = [];
+
+    while(queue.length){
+        const {node, level} = queue.pop();
+        if(node == null) continue;
+        if(result.length < level + 1)
+            result.push([]);
+        result[level].push(node.val);
+        queue.push({node: node.right, level: level + 1});
+        queue.push({node: node.left, level: level + 1});
+    }
+    
     return result;
 };
