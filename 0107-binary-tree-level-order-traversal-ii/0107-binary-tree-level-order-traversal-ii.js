cc877466a490df348bd6f72314d215c8b9fcc71a
Michael <mic3ael@gmail.com>
2024-08-12 11:03:31 +0300

    Time: 70 ms (16.72%), Space: 52.3 MB (28.36%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.js
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.js
@@ -10,26 +10,26 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrderBottom2 = function(root) {
+var levelOrderBottom = function(root) {
     const stack = [{node: root, level: 0}];
     const result = new Array();
 
     while(stack.length){
-        const { node, level } = stack.pop();
+        const { node, level } = stack.shift();
         if(node == null) continue;
         
         if(result.length < level + 1)
             result.push([]);
         result[level].push(node.val);
 
-        stack.push({node: node.right, level: level+1});
         stack.push({node: node.left, level: level+1});
+        stack.push({node: node.right, level: level+1});
     }
 
     return result.reverse();
 };
 
-var levelOrderBottom = function(root) {
+var levelOrderBottom2 = function(root) {
     const valuesByLevel = combineByLevel(root);
     return valuesByLevel.reverse();
 }
