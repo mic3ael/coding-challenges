636162e2f60e2a675b3b5985e57abe2d298c85ba
Michael <mic3ael@gmail.com>
2024-08-13 19:56:58 +0300

    Time: 62 ms (28.76%), Space: 51 MB (13.51%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+     if (cost.length < 2) return -1;
+    let first = cost[0];
+    let second = cost[1];
+
+    for (let i = 2; i < cost.length; i++) {
+        const element = cost[i];
+        current = element + Math.min(first, second);
+        
+        [first, second] = [second, current];
+    }
+
+    return Math.min(first, second);
+};
