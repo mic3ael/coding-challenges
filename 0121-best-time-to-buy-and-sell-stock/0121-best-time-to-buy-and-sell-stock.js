047e0a8d28fa19871146d178a3d3b776a6e3dd95
Michael <mic3ael@gmail.com>
2024-08-13 12:01:05 +0300

    Time: 74 ms (59.12%), Space: 59.5 MB (21.21%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let mProfit = 0;
+    let leftIdx = 0;
+    let rightIdx = 0;
+
+    while(rightIdx < prices.length){
+        const profit = prices[rightIdx] - prices[leftIdx];
+        if(profit < 0){
+            leftIdx++;
+            continue;
+        } 
+        
+        rightIdx++;
+        mProfit = Math.max(mProfit, profit);
+    }
+    
+    // console.log("a: ", prices[rightIdx] - prices[leftIdx]);
+    return mProfit;
+};
