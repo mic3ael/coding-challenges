421f91a5f7f98f1358e25dddbb8f37fbbfaba4fe
Michael <mic3ael@gmail.com>
2024-08-19 00:01:44 +0300

    Time: 43 ms (96.14%), Space: 49.4 MB (46.57%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let sIdx = 0;
+
+    for(let i = 0; i < t.length; i++){
+        if(s.charAt(sIdx) == t.charAt(i)) sIdx++;
+    }
+
+    return sIdx >= s.length;
+};
