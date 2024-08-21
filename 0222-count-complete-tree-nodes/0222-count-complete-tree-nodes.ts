bc65a08fddd604552592c308ccf1d1ca49985c71
Michael <mic3ael@gmail.com>
2024-08-21 11:17:05 +0300

    Time: 75 ms (81.18%), Space: 67.8 MB (99.44%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.ts
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+var countNodes = function(root) {
+    if(root == null) return 0;
+    return countNodes(root.left) + countNodes(root.right) + 1;
+};
