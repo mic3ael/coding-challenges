d8ae10f946cc88f5670e17d6aad5090056d81075
Michael <mic3ael@gmail.com>
2024-09-04 16:48:47 +0300

    Time: 142 ms (14.21%), Space: 86.4 MB (5.09%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -7,14 +7,14 @@ var containsNearbyDuplicate = function(nums, k) {
     const occurs = new Map();
     for(let i = 0; i < nums.length; i++) {
         if(!occurs.has(nums[i])) occurs.set(nums[i], []);
-        occurs.get(nums[i]).push(i);
-    }
-    for(let [num, indexes] of occurs){
+        const indexes = occurs.get(nums[i]);
+        indexes.push(i);
         if(indexes.length < 2) continue;
+    
         for(let i = 1; i < indexes.length; i++){
             if(Math.abs(indexes[i-1] - indexes[i]) <= k) return true;
         }
     }
-
+    
     return false;
 };
