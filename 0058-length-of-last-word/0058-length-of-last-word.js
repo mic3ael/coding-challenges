2a71d887831cee08226e01faee126dcc04c8f602
Michael <mic3ael@gmail.com>
2024-09-05 23:20:06 +0300

    Time: 51 ms (58.51%), Space: 48.7 MB (78.17%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,22 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let lastLen = 0;
-    let currentLen = 0;
-
-    for(let i = 0; i < s.length; i++){
-        if(s.charAt(i) == ' '){
-            lastLen = currentLen;           
-            currentLen = 0;
-            while(s.charAt(i) == ' '){
-                i++;
-            }
-            if(i >= s.length) break;
-        }
-
-        currentLen++;
-    }
-    
-    if(currentLen) return currentLen;
-    return lastLen;
+    const words = s.trim().split(' ');
+    return words[words.length-1].length;
 };
