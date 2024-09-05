3b719f5afeb6fc5c31e3dafc08b8ca23d7e15a71
Michael <mic3ael@gmail.com>
2024-09-06 00:22:57 +0300

    Time: 183 ms (7.24%), Space: 68.8 MB (29.09%) - LeetHub

---
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxLevelSum = function(root) {
+    const queue = [root];
+    let levelNodes = 1;
+    let level = 1;
+    const result = { maxSum: -Infinity, level: 0};
+    let currentSum = 0;
+
+    while(queue.length){
+        const node = queue.shift();
+        levelNodes--;
+        currentSum += node.val;
+        if(node.left)
+            queue.push(node.left);
+        if(node.right)
+            queue.push(node.right);
+
+        if(levelNodes == 0) {
+           if(result.maxSum < currentSum){
+                result.maxSum = currentSum;
+                result.level = level;
+           }
+
+           level++;
+           currentSum = 0;
+           levelNodes = queue.length;
+        }
+    }
+
+    return result.level;
+};
