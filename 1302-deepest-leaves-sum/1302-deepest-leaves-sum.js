dc9a94405d444a481ef8a039321b4dc5907cfd3d
Michael <mic3ael@gmail.com>
2024-09-10 21:46:59 +0300

    Time: 132 ms (79.24%), Space: 70.1 MB (53.29%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var deepestLeavesSum = function(root) {
+    const queue = [root];
+    let levelNodes = 1;
+    let levelSum = 0;
+    let deepestLevelSum = 0;
+    
+    while(queue.length){
+        const node = queue.shift();
+        levelNodes--;
+        levelSum += node.val;
+
+        if(node.left)
+            queue.push(node.left);
+        if(node.right)
+            queue.push(node.right);
+
+        if(levelNodes == 0){
+            levelNodes = queue.length;
+            deepestLevelSum = levelSum;
+            levelSum = 0;
+        }
+    }
+
+    return deepestLevelSum;
+};
