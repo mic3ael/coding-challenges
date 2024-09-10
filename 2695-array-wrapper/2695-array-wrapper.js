05c4de8e16df6c953d9733886282899459eed47f
Michael <mic3ael@gmail.com>
2024-09-10 23:22:57 +0300

    Time: 40 ms (98.13%), Space: 50 MB (73.73%) - LeetHub

---
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -0,0 +1,33 @@
+class ArrayWrapper {
+    
+    constructor(nums) {
+        this.data = nums;
+    }
+    
+    valueOf() {
+        let sum = 0;
+        for(let i = 0; i < this.data.length; i++){
+            sum += this.data[i];
+        }
+        
+        return sum;
+    }
+    
+    toString() {
+        if(this.data.length == 0) return `[]`;
+        let result = `[`;
+        for(let i = 0; i < this.data.length - 1; i++){
+            result += `${this.data[i]},`;
+        }
+        result += `${this.data[this.data.length - 1]}]`;
+        return result;
+    }
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
