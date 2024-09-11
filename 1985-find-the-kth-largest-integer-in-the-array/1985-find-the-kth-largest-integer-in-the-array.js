f136702e00df38cf47d3653df2300ce974a5dbf7
Michael <mic3ael@gmail.com>
2024-09-11 11:50:50 +0300

    Time: 123 ms (56.7%), Space: 62.3 MB (72.17%) - LeetHub

---
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.js
@@ -0,0 +1,82 @@
+class MaxHeap {
+    #data;
+    constructor(){
+        this.#data = [];
+    }
+    #hasParent(idx){
+        const parentIdx = Math.floor((idx - 1) / 2);
+        if(parentIdx < 0) return false;
+        return true;
+    }
+    #hasLeftChild(idx){
+        const leftIdx = idx * 2 + 1;
+        if(leftIdx >= this.#data.length) return false;
+        return true;
+    }
+    #hasRightChild(idx){
+        const rightIdx = idx * 2 + 2;
+        if(rightIdx >= this.#data.length) return false;
+        return true;
+    }
+    #getParentIdx(idx){
+        return Math.floor((idx-1)/2);
+    }
+    #getLeftChildIdx(idx){
+        return idx * 2 + 1;
+    }
+    #getRightChildIdx(idx){
+        return idx * 2 + 2;
+    }
+    #isEmpty(){
+        return this.#data.length == 0;
+    }
+    #swap(idx, idx2){
+        const temp = this.#data[idx];
+        this.#data[idx] = this.#data[idx2];
+        this.#data[idx2] = temp;
+    }
+    add(value){
+        this.#data.push(BigInt(value));
+        let idx = this.#data.length - 1;
+        while(this.#hasParent(idx)){
+            const parentIdx = this.#getParentIdx(idx);
+            if(this.#data[parentIdx] > this.#data[idx]) break;
+            this.#swap(parentIdx, idx);
+            idx = parentIdx;
+        }
+    }
+    delete(){
+        const item = this.#data[0];
+        this.#data[0] = this.#data.pop();
+        let idx = 0;
+        while(this.#hasLeftChild(idx)){
+            let largestIdx = this.#getLeftChildIdx(idx);
+            if(this.#hasRightChild(idx) && this.#data[this.#getRightChildIdx(idx)] > this.#data[largestIdx])
+                largestIdx = this.#getRightChildIdx(idx);
+            if(this.#data[largestIdx] < this.#data[idx]) break;
+            this.#swap(largestIdx, idx);
+            idx = largestIdx;
+        }
+        return item.toString();
+    }
+}
+
+/**
+ * @param {string[]} nums
+ * @param {number} k
+ * @return {string}
+ */
+var kthLargestNumber = function(nums, k) {
+    const maxHeap = new MaxHeap();
+    for(let i = 0; i < nums.length; i++) {
+        maxHeap.add(nums[i]);
+    }
+
+    let result;
+    while(k > 0){
+        result = maxHeap.delete();
+        k--;
+    }
+
+    return result;
+};
