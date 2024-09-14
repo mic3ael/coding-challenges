79fd0238cd18d8330026cb437d3127a48040a406
Michael <mic3ael@gmail.com>
2024-09-14 11:01:30 +0300

    Time: 481 ms (27.5%), Space: 63.6 MB (57.5%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.js
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.js
@@ -4,13 +4,24 @@
  * @return {number[]}
  */
 var xorQueries = function(arr, queries) {
+    const prefixSum = new Array(arr.lenght);
+    let sum = 0;
+    
+    for(let i = 0; i < arr.length; i++){
+        sum ^= arr[i];
+        prefixSum[i] = sum;
+    }   
+    
+    
     const result = new Array(queries.length);
+    
     for(let i = 0; i < queries.length; i++){
         let [leftIdx, rightIdx] = queries[i];
-        let current = 0;
-        while(leftIdx <= rightIdx){
+        let current = prefixSum[rightIdx];
+        leftIdx--;
+        while(leftIdx >= 0){
             current ^= arr[leftIdx];
-            leftIdx++;
+            leftIdx--;
         }
         result[i] = current;
     }
