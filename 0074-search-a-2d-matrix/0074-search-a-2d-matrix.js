87d9d08837492e13eb1a3b7dbb33420d3a8bfc12
Michael <mic3ael@gmail.com>
2024-09-19 21:58:05 +0300

    Time: 65 ms (5.33%), Space: 48.4 MB (95.26%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    if(matrix.length == 0) return false;
+
+    let row = 0;
+    let left = 0;
+    let right = matrix[row].length - 1;
+    while(left <= right && row <= matrix.length - 1) {
+        let mid = Math.floor(( left + right ) / 2);
+        const val = matrix[row][mid];
+        if(val == target) return true;
+        if(val < target) {
+            left = mid + 1;
+            if(left > right) {
+                row++;
+                left = 0;
+                if(row > matrix.length - 1) break;
+                right = matrix[row].length - 1;
+            }
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return false;
+};
