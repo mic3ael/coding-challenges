c07bf8932eb0a5dc102c9ff2a95d0e7e51188c06
Michael <mic3ael@gmail.com>
2024-09-23 12:03:38 +0300

    Time: 57 ms (51.35%), Space: 49.8 MB (70.31%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+     if (cost.length < 2) return -1;
+    let first = cost[0];
+    let second = cost[1];
+
+    for (let i = 2; i < cost.length; i++) {
+        const element = cost[i];
+        const current = element + Math.min(first, second);
+        first = second;
+        second = current;
+    }
+
+    return Math.min(first, second);
+};
+
+var minCostClimbingStairs = function(cost) {
+    const index = cost.length;
+    const memo = new Map();
+    return Math.min(minCost(cost, index - 1, memo), minCost(cost, index - 2, memo));
+}
+
+function minCost(cost, index, memo){
+    if(index < 2) return cost[index];
+    if(memo.has(index)) return memo.get(index);
+
+    cost[index] = cost[index] + Math.min(minCost(cost, index - 1, memo), minCost(cost, index - 2, memo));
+    memo.set(index, cost[index]);
+    return cost[index];
+}
